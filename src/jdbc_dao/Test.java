78887b35013c992e3543b1dcee907ecc9f6a7ab9
amandhakad13 <ad5538694@gmail.com>
2025-07-09 09:07:40 -0700

    add scanner to input from console

--- src/jdbc_dao/Test.java
+++ src/jdbc_dao/Test.java
@@ -1,19 +1,32 @@
 package jdbc_dao;
 
 import java.sql.SQLException;
+import java.util.Scanner;
 
 public class Test {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		Student s1 = new Student();
 		StudentDAO sdao = new StudentDAO();
 		
-		s1.setId(6);
-		s1.setName("Shekhar");
-		s1.setMobileNo(123565478);
-		s1.setAge(45);
-		s1.setDOB("2007-06-12");
+		System.out.print("Enter id : ");
+		int id = sc.nextInt();
+		System.out.print("Enter name : ");
+		String name = sc.next();
+		System.out.print("Enter mobile no : ");
+		int mobileNo = sc.nextInt();
+		System.out.print("Enter age : ");
+		int age = sc.nextInt();
+		System.out.print("Enter dob : ");
+		String dob = sc.next();
+		
+		s1.setId(id);
+		s1.setName(name);
+		s1.setMobileNo(mobileNo);
+		s1.setAge(age);
+		s1.setDOB(dob);
 
 		int isInserted = 0;
 		
@@ -29,6 +42,8 @@ public class Test {
 		else {
 			System.out.println("Record Not Inserted");
 		}
+		
+		sc.close();
 	}
 
 }
